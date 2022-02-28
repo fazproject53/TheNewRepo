b5e7ec9019e95be9be6d147f097293714f4d8929
AlaaKhaledAhmed <97679146+AlaaKhaledAhmed@users.noreply.github.com>
2022-02-28 14:30:23 +0300

    add slider image

---
+++ .history/celepraty-Alaa/lib/Models/Methods/classes/GradientIcon_20220228142220.dart
@@ -0,0 +1,40 @@
+import 'package:celepraty/Models/Variabls/varaibles.dart';
+import 'package:flutter/material.dart';
+
+class GradientIcon extends StatelessWidget{
+
+  GradientIcon(
+      this.icon,
+      this.size,
+      this.gradient,
+      {this.onPressed}
+      );
+
+  final IconData icon;
+  final double size;
+  final Gradient gradient;
+  Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      child: SizedBox(
+          width: size* 1.2,
+          height: size*1.2,
+          child: IconButton(
+            icon: Icon(icon, size : size, color: white,),
+             onPressed:(){}
+          )
+          
+          
+
+
+      ),
+      shaderCallback: (Rect bounds){
+        final Rect rect = Rect.fromLTRB(0,0,size,size);
+        return gradient.createShader(rect);
+      },
+    );
+  }
+
+}
