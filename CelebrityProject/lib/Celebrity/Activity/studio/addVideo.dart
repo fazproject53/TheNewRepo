d9619771cf7efba1a3a1dfd5774465e7c4b4d5ee
Fayzeh-2019 <Fayzeh_Alnabulsi@hotmail.com>
2022-04-25 12:07:16 +0300

    التفاعلات دن

--- CelebrityProject/lib/Celebrity/Activity/studio/addVideo.dart
+++ CelebrityProject/lib/Celebrity/Activity/studio/addVideo.dart
@@ -4,7 +4,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:celepraty/Models/Methods/method.dart';
 import 'package:celepraty/Models/Variables/Variables.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as Path;
+import 'package:path_provider/path_provider.dart';
+import '../activity_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -22,6 +29,7 @@ class _addVideoState extends State<addVideo> {
   TextEditingController controlvideodesc = new TextEditingController();
 
   File? studioVideo;
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -30,26 +38,55 @@ class _addVideoState extends State<addVideo> {
         body: SafeArea(
             child: SingleChildScrollView(
                 child: Container(
-                    child: Form(        key: _formKey,
+                    child: Form(key: _formKey,
 
                       child: paddingg(12, 12, 5, Column(
                           crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
-                            padding(10, 12, Container( alignment : Alignment.topRight,child:  Text(' اضافة فيديو جديد', style: TextStyle(fontSize: 18.sp, color: textBlack , fontFamily: 'Cairo'), )),),
+                            padding(10, 12, Container(
+                                alignment: Alignment.topRight, child: Text(
+                              ' اضافة فيديو جديد', style: TextStyle(
+                                fontSize: 18.sp,
+                                color: textBlack,
+                                fontFamily: 'Cairo'),)),),
 
                             SizedBox(height: 20.h,),
 
-                            paddingg(15, 15, 12,textFieldNoIcon(context, 'عنوان الفيديو', 14, false, controlvideotitle,(String? value) {if (value == null || value.isEmpty) {
-                              return 'Please enter some text';} return null;},false),),
-                            paddingg(15, 15, 12,textFieldDesc(context, 'وصف الفيديو', 14, false, controlvideodesc,(String? value) {if (value == null || value.isEmpty) {
-                              return 'Please enter some text';} return null;}),),
+                            paddingg(15, 15, 12, textFieldNoIcon(
+                                context,
+                                'عنوان الفيديو',
+                                14,
+                                false,
+                                controlvideotitle, (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                                false),),
+                            paddingg(15, 15, 12, textFieldDesc(
+                                context, 'وصف الفيديو', 14, false,
+                                controlvideodesc, (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            }),),
 
 
                             SizedBox(height: 20.h),
-                            paddingg(15, 15, 12, uploadImg(200, 54,text(context, 'قم برفع فيديو ', 12, black),(){pickImage(image);}),),
+                            paddingg(15, 15, 12, uploadImg(200, 54,
+                                text(context, 'قم برفع فيديو ', 12, black), () {
+                                  getVideo(context);
+                                }),),
 
                             SizedBox(height: 20.h),
-                            padding(15, 15, gradientContainerNoborder(getSize(context).width,  buttoms(context, 'اضافة ', 15, white, (){goTopageReplacement(context, ActivityScreen()) ;})),),
+                            padding(15, 15, gradientContainerNoborder(getSize(
+                                context).width,
+                                buttoms(context, 'اضافة ', 15, white, () {
+                                  addvideo();
+                                  goTopageReplacement(context, ActivityScreen()) ;
+                                })),),
                             const SizedBox(height: 30,),
                           ]),
                       ),
@@ -60,36 +97,70 @@ class _addVideoState extends State<addVideo> {
       ),
     );
   }
-  Future<http.Response> addVideo() async {
+
+  addvideo() async {
     String token2 =
         'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMWNjMTA1MjcxODRhN2QzYTIwNDJkYTdmNTMyNTA4ZTdjMDE4NWQwOWI3MzRkY2VhMGEzZjYxY2U3MjRmYmM4M2M5ZTcwNGYyYmNjYmIwNjAiLCJpYXQiOjE2NTA3MzU4NjEuMDQ2MzQwOTQyMzgyODEyNSwibmJmIjoxNjUwNzM1ODYxLjA0NjM0NTk0OTE3Mjk3MzYzMjgxMjUsImV4cCI6MTY4MjI3MTg2MS4wNDEzODQ5MzUzNzkwMjgzMjAzMTI1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.Dp0SkGykv6S4A3maaSmgkWMfKCXbumkxg8cmCJcgoBmFOjxwiKht2vywEnOXtPyFSV7BY48IpvrO6xlrfCE9Oy5wSp86caE-uYEo-uHgzsuRqf-hHpL9DcNsrfuZBQi4h3GiiCfyYV0362FkVl_hclamppj8VL-S7C02_Ddg7Ygnlce3-k8Hm8NRutBREsk4or75C-1mL70ArOCWuLazaUbAJexN2MuLMjqQF9h-pgcaQhEY3rhBEarcEcfdREJgGy_5zARbAdSi_mwclQCqNr9KatmRhkDL_My0GqmGvkt8RUSb_Uo93NXv9lvdw41gMcrStKvVbGg4RMRSxPRD_P9I8-26Ipasx5wMlFdZU10-mSrGKDLu3d4vxVcLFcQIwQqK19m5urFdgMVznRBqEnqceQUb_UjUh8i7VOa8rRUFFbLy7ALZaAk23DtVz25AHRIaYbV_jmgXbx_9IuJc3-dslYVvfGbtgRniKgLEDHKgWgVfiljUU9aUZmIh7i1uYgDZm1LBDWY_wPRQdPoQedfSiLs0Qy1ZmfahRBRWgNKFZDMoKvhtbuuP3rJLcIiQ'
         '6nLbyu1i4ma8ly74po4bYZcQDKTFx1oSi2fkUkDF_ZtqLFnx7xvzfXY-Za8krfB-AThg7Phi8-KVCdlTza_KwqkQciGzG4MD3-eY62JXIIU';
-    final response = await http.post(
-      Uri.parse(
-        'https://mobile.celebrityads.net/api/celebrity/news/add',
-      ),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $token2'
-      },
-      body: jsonEncode(<String, dynamic>{
-        'title': controlvideotitle.text,
-        'description': controlvideodesc.text,
-        'image':studioVideo!,
-        'type':'image',
-      }),
-    );
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
 
-      print(response.body);
-      return response;
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load activity');
+
+    var stream = new http.ByteStream(
+        DelegatingStream.typed(studioVideo!.openRead()));
+    // get file length
+    var length = await studioVideo!.length();
+
+    // string to uri
+    var uri =
+    Uri.parse("https://mobile.celebrityads.net/api/celebrity/studio/add");
+
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token2"
+    };
+    // create multipart request
+    var request = new http.MultipartRequest("POST", uri);
+
+    // multipart that takes file
+    var multipartFile = new http.MultipartFile('image', stream, length,
+        filename: basename(studioVideo!.path));
+
+    // add file to multipart
+    request.files.add(multipartFile);
+    request.headers.addAll(headers);
+    request.fields["title"] = controlvideotitle.text;
+    request.fields["description"] = controlvideodesc.text;
+    request.fields["type"] = "vedio";
+    // send
+    var response = await request.send();
+    print(response.statusCode);
+
+    // listen for response
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+    });
+  }
+
+  Future<File?> getVideo(context) async {
+    PickedFile? pickedFile =
+    await ImagePicker.platform.pickVideo(source: ImageSource.gallery);
+    if (pickedFile == null) {
+      return null;
     }
+    final File file = File(pickedFile.path);
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    final String fileName = Path.basename(pickedFile.path);
+   final String fileExtension = Path.extension(fileName);
+    File newVideo = await file.copy('$path/$fileName');
+    setState(() {
+      if(fileExtension == ".mp4"){
+        studioVideo = newVideo;
+      }else{ ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(
+        content: Text(
+            "يجب ان يكون امتداد الفديو .mp4",style: TextStyle(color: Colors.red)),
+      ));}
+
+    });
   }
 }
--- CelebrityProject/lib/Celebrity/Activity/studio/addphoto.dart
+++ CelebrityProject/lib/Celebrity/Activity/studio/addphoto.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:celepraty/Celebrity/Activity/studio/studio.dart';
@@ -12,6 +13,9 @@ import 'package:path/path.dart' as Path;
 import '../activity_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+
 
 class addphoto extends StatefulWidget{
 
@@ -49,11 +53,12 @@ class _addphotoState extends State<addphoto> {
 
 
                             SizedBox(height: 20.h),
-                            paddingg(15, 15, 12, uploadImg(200, 54,text(context, 'قم برفع صورة ', 12, black),(){getPhoto();}),),
+                            paddingg(15, 15, 12, uploadImg(200, 54,text(context, 'قم برفع صورة ', 12, black),(){getPhoto(context);}),),
 
                             SizedBox(height: 20.h),
                             padding(15, 15, gradientContainerNoborder(getSize(context).width,  buttoms(context, 'اضافة ', 15, white, (){
-                              addPhoto();})),),
+                              addPhoto();
+                              goTopageReplacement(context, ActivityScreen()) ;})),),
                             const SizedBox(height: 30,),
                           ]),
                       ),
@@ -69,37 +74,45 @@ class _addphotoState extends State<addphoto> {
     String token2 =
         'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMWNjMTA1MjcxODRhN2QzYTIwNDJkYTdmNTMyNTA4ZTdjMDE4NWQwOWI3MzRkY2VhMGEzZjYxY2U3MjRmYmM4M2M5ZTcwNGYyYmNjYmIwNjAiLCJpYXQiOjE2NTA3MzU4NjEuMDQ2MzQwOTQyMzgyODEyNSwibmJmIjoxNjUwNzM1ODYxLjA0NjM0NTk0OTE3Mjk3MzYzMjgxMjUsImV4cCI6MTY4MjI3MTg2MS4wNDEzODQ5MzUzNzkwMjgzMjAzMTI1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.Dp0SkGykv6S4A3maaSmgkWMfKCXbumkxg8cmCJcgoBmFOjxwiKht2vywEnOXtPyFSV7BY48IpvrO6xlrfCE9Oy5wSp86caE-uYEo-uHgzsuRqf-hHpL9DcNsrfuZBQi4h3GiiCfyYV0362FkVl_hclamppj8VL-S7C02_Ddg7Ygnlce3-k8Hm8NRutBREsk4or75C-1mL70ArOCWuLazaUbAJexN2MuLMjqQF9h-pgcaQhEY3rhBEarcEcfdREJgGy_5zARbAdSi_mwclQCqNr9KatmRhkDL_My0GqmGvkt8RUSb_Uo93NXv9lvdw41gMcrStKvVbGg4RMRSxPRD_P9I8-26Ipasx5wMlFdZU10-mSrGKDLu3d4vxVcLFcQIwQqK19m5urFdgMVznRBqEnqceQUb_UjUh8i7VOa8rRUFFbLy7ALZaAk23DtVz25AHRIaYbV_jmgXbx_9IuJc3-dslYVvfGbtgRniKgLEDHKgWgVfiljUU9aUZmIh7i1uYgDZm1LBDWY_wPRQdPoQedfSiLs0Qy1ZmfahRBRWgNKFZDMoKvhtbuuP3rJLcIiQ'
         '6nLbyu1i4ma8ly74po4bYZcQDKTFx1oSi2fkUkDF_ZtqLFnx7xvzfXY-Za8krfB-AThg7Phi8-KVCdlTza_KwqkQciGzG4MD3-eY62JXIIU';
-    final response = await http.post(
-      Uri.parse(
-        'https://mobile.celebrityads.net/api/celebrity/studio/add',
-      ),
 
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $token2'
-      },
-      body: jsonEncode(<String, dynamic>{
-        'title':controlphototitle.text,
-        'description':controlphotodesc.text,
-        'image':studioimage,
-        'type':'image'
-      }),
-    );
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
 
-      print(response.body);
-      return response;
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load activity');
-    }
+    var stream = new http.ByteStream(DelegatingStream.typed(studioimage!.openRead()));
+    // get file length
+    var length = await studioimage!.length();
+
+    // string to uri
+    var uri =
+    Uri.parse("https://mobile.celebrityads.net/api/celebrity/studio/add");
+
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token2"
+    };
+    // create multipart request
+    var request = new http.MultipartRequest("POST", uri);
+
+    // multipart that takes file
+    var multipartFile = new http.MultipartFile('image', stream, length,
+        filename: basename(studioimage!.path));
+
+    // add file to multipart
+    request.files.add(multipartFile);
+    request.headers.addAll(headers);
+    request.fields["title"] = controlphototitle.text;
+    request.fields["description"] = controlphotodesc.text;
+    request.fields["type"] = "image";
+    // send
+    var response = await request.send();
+    print(response.statusCode);
+
+    // listen for response
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+    });
+
   }
 
-  Future<File?> getPhoto() async {
+  Future<File?> getPhoto(context) async {
     PickedFile? pickedFile =
     await ImagePicker.platform.pickImage(source: ImageSource.gallery);
     if (pickedFile == null) {
@@ -109,10 +122,17 @@ class _addphotoState extends State<addphoto> {
     final Directory directory = await getApplicationDocumentsDirectory();
     final path = directory.path;
     final String fileName = Path.basename(pickedFile.path);
-// final String fileExtension = extension(image.path);
+    final String fileExtension = Path.extension(fileName);
     File newImage = await file.copy('$path/$fileName');
     setState(() {
-      studioimage = newImage;
+      if(fileExtension == ".mp4"){
+        studioimage = newImage;
+      }else{ ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(
+        content: Text(
+            "امتداد الصورة المسموح به jpg, png",style: TextStyle(color: Colors.red)),
+      ));}
+
     });
   }
 }
