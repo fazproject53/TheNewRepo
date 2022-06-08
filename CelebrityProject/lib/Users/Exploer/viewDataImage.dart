c481cf71a5fd88a8d103b84c6832aa4f286a3b02
fazproject53 <fazproject53@gmail.com>
2022-06-08 13:01:41 +0300

    fex

--- CelebrityProject/lib/Users/Exploer/viewDataImage.dart
+++ CelebrityProject/lib/Users/Exploer/viewDataImage.dart
@@ -4,9 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../Models/Methods/method.dart';
 import '../../Models/Variables/Variables.dart';
 
-
 class ImageData extends StatefulWidget {
-
   final String? image;
   const ImageData({Key? key, this.image}) : super(key: key);
 
@@ -15,53 +13,26 @@ class ImageData extends StatefulWidget {
 }
 
 class _ImageDataState extends State<ImageData> {
-
   bool clicked = false;
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
-      textDirection: TextDirection.rtl,
-      child:  Scaffold(
-        backgroundColor: black.withOpacity(0.80),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 30.h, right: 20.w),
-              child: Row(
-                children: [
-                  ///back icon to the main screen
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back_ios),
-                    color: white,
-                    iconSize: 30,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                InkWell(
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                          margin: EdgeInsets.only(
-                              top: getSize(context).height/9),
-                          height: 400.h,
-                          child: widget.image == null ? Text('No Photo To Show') :  Image.network(widget.image!)),
-
-                    ],
-                  ),
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+            appBar: drowAppBar("", context),
+            //backgroundColor: black.withOpacity(0.80),
+            body: Container(
+              // color: red,
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: NetworkImage(
+                  widget.image!,
                 ),
-              ],
-            ),
-          ],
-        ), ),
-    );
+                fit: BoxFit.contain,
+              )),
+            )));
   }
 }
