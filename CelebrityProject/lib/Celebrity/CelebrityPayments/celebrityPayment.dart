9b6f06261d4aeb17c89c06c6ec10de8a4374d9dd
fazit <fazproject53@gmail.com>
2022-06-12 11:31:45 +0300

    celebrityPayments

---
+++ CelebrityProject/lib/Celebrity/CelebrityPayments/celebrityPayment.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CelebrityPayments extends StatefulWidget {
+  const CelebrityPayments({Key? key}) : super(key: key);
+
+  @override
+  _CelebrityPaymentsState createState() => _CelebrityPaymentsState();
+}
+
+class _CelebrityPaymentsState extends State<CelebrityPayments> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
