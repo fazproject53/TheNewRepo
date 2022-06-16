3f81cd7c23ede230df06d2502429d79f0af3e2b8
fazproject53 <fazproject53@gmail.com>
2022-06-16 09:16:10 +0300

    splash screen

--- CelebrityProject/lib/main.dart
+++ CelebrityProject/lib/main.dart
@@ -1,11 +1,5 @@
-///import section
-import 'package:celepraty/Account/Singup.dart';
+
 import 'package:celepraty/Account/logging.dart';
-import 'package:celepraty/Celebrity/Activity/news/addNews.dart';
-import 'package:celepraty/Celebrity/blockList.dart';
-import 'package:celepraty/Celebrity/celebrityHomePage.dart';
-import 'package:celepraty/Celebrity/setting/celebratyProfile.dart';
-import 'package:celepraty/Celebrity/setting/profileInformation.dart';
 import 'package:celepraty/MainScreen/main_screen_navigation.dart';
 import 'package:celepraty/Models/Methods/method.dart';
 import 'package:celepraty/Models/Variables/Variables.dart';
@@ -13,16 +7,7 @@ import 'package:celepraty/introduction_screen/ModelIntro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'Account/LoggingSingUpAPI.dart';
-import 'Account/ResetPassword/PasswordCoding.dart';
-import 'Account/ResetPassword/ResetNewPassword.dart';
-import 'Account/ResetPassword/SendEmailGUI.dart';
-import 'Celebrity/Balance/radioListTile.dart';
-import 'Celebrity/Calendar/calendar_main.dart';
-import 'Celebrity/HomeScreen/celebrity_home_page.dart';
-import 'Users/Exploer/Explower.dart';
-import 'celebrity/DiscountCodes/discount_codes_main.dart';
 import 'introduction_screen/introduction_screen.dart';
 
 int? initScreen;
